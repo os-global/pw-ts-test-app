68b5a9583e8784fe58f00316c531bbc52b70317a
os-global <sasha.susla@gmail.com>
2024-01-30 02:03:58 +0200

    logout test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,13 @@
-import test from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("login", async ({ page }) => {
-    
+test('login/logout test', async ({ page }) => {
+  await page.goto('http://localhost:8000');
+  await page.getByLabel('Username:').fill('default');
+  await page.getByLabel('Password:').fill('QADqwerty');
+  const loginButton = page.getByRole('button', { name: 'Login' })
+  await loginButton.click();
+  const logout = page.locator('.logOut');
+  await expect(logout).toBeVisible();
+  await page.locator('.logOut').click();
+  await expect(loginButton).toBeVisible();
+});
