139b10ae58b62c0df7655b0e946aa7fa980d7200
os-global <sasha.susla@gmail.com>
2024-01-30 02:04:16 +0200

    remove draft

--- tests/test-1.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:8000');
-  await page.getByLabel('Username:').fill('default');
-  await page.getByLabel('Password:').fill('QADqwerty');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
-});
