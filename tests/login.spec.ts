54b2910854048f85d599fea6a81c76d8b0185ba3
os-global <sasha.susla@gmail.com>
2024-01-31 02:24:14 +0200

    Add test for user login with wrong credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -11,3 +11,12 @@ test('login/logout test', async ({ page }) => {
   await page.locator('.logOut').click();
   await expect(loginButton).toBeVisible();
 });
+
+test('user cannot login with wrong credentials', async ({ page }) => {
+  await page.goto('http://localhost:8000');
+  await page.getByLabel('Username:').fill('default');
+  await page.getByLabel('Password:').fill('wrong password');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.locator('.logOut')).toBeHidden();
+  await expect(page.getByText("Your username and password didn't match. Please try again.")).toBeVisible();
+});
