72e083b3295d40f3bc4d8471c4e4aba5708b0941
os-global <sasha.susla@gmail.com>
2024-05-03 00:45:29 +0300

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,9 +22,7 @@ export default defineConfig<TestOptions>({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["blob", {
-    fileName: 'report.zip'
-  }], ["./support/reporters/pwServer"]],
+  reporter: [["html", { open: "always" }]],
   
   // [
   //   [
