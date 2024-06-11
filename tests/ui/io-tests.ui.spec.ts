254c35f3e89f7337746a7326b9a8eb10315cf95d
os-global <sasha.susla@gmail.com>
2024-06-12 00:20:04 +0300

    upload test

--- tests/ui/io-tests.ui.spec.ts
+++ tests/ui/io-tests.ui.spec.ts
@@ -12,12 +12,20 @@ test(
   }
 );
 
-test(
+test.only(
   "upload tests",
   { tag: ["@ui", "@regression"] },
   async ({ newUserApp: { testCases }, uploadTestsFilePath }) => {
     await testCases.open();
     const initialTestsCount = await testCases.getTestsHeaderCount();
+    console.log(`File to upload ${uploadTestsFilePath}`)
+    // list files in ./tmp directory
+    const fs = require('fs');
+    fs.readdir('./tmp', (err, files) => {
+      files.forEach(file => {
+        console.log(file);
+      });
+    });
     await testCases.uploadTests(uploadTestsFilePath);
     const csvLineCount = await new IoHelper().countLines(uploadTestsFilePath);
     await testCases.verifyTestsHeaderCount(initialTestsCount + csvLineCount);
