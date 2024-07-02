15215ce5fd5e7868fa31fb5e6965fdbd30e9f146
os-global <sasha.susla@gmail.com>
2024-07-02 23:57:42 +0300

    reorginize stats tests

--- tests/ui/stats.ui.spec.ts
+++ tests/ui/stats.ui.spec.ts
@@ -1,10 +1,29 @@
 import { test } from "../../src/fixtures";
 
-
-test("verify stats on dashboard", async ({
+test("verify dashboard stats", async ({
   defaultUserApp: { dashboard },
   mockStats,
 }) => {
   await dashboard.open();
-  await dashboard.verifyStats(mockStats.total, mockStats.passed, mockStats.failed, mockStats.norun);
+  await dashboard.verifyStats(
+    mockStats.total,
+    mockStats.passed,
+    mockStats.failed,
+    mockStats.norun
+  );
+});
+
+test("verify rating stats", async ({
+  defaultUserApp: { rating },
+  mockRating,
+}) => {
+  await rating.open();
+  await rating.verifyAuthorsRating(
+    mockRating.top_1_author.name,
+    mockRating.top_1_author.test_case_count,
+    mockRating.top_2_author.name,
+    mockRating.top_2_author.test_case_count,
+    mockRating.top_3_author.name,
+    mockRating.top_3_author.test_case_count
+  );
 });
