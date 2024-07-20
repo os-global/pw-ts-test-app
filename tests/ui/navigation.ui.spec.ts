3ae16989224146ef110065cff8e93d1d52910268
os-global <sasha.susla@gmail.com>
2024-07-20 23:58:02 +0300

    navigation tests per each menu item

--- tests/ui/navigation.ui.spec.ts
+++ tests/ui/navigation.ui.spec.ts
@@ -1,16 +1,22 @@
-import { test } from '@fixtures/.';
+import { test } from "@fixtures/.";
 
 const testCases = [
-    // {title: 'Dashboard', path: '/'},
-    {title: 'Rating', path: '/rating/'},
-    // {title: 'Test Cases', path: '/tests/'},
+  { title: "Dashboard", path: "/" },
+  { title: "Rating", path: "/rating/" },
+  { title: "Test Cases", path: "/tests/" },
+  { title: "Test Runs", path: "/runs/" },
+  { title: "Create new test", path: "/test/new/" },
+  { title: "Demo pages", path: "/demoPages/" },
+  { title: "Demo controls", path: "/demoControls/" },
+];
 
-]
-
-for (const {title, path} of testCases) {
-    test(`verify navigation to '${title}' page`, async ({defaultUserApp, baseURL}) => {
-        await defaultUserApp.navigate.goto("/");
-        await defaultUserApp.navigate.openMenuItem(title);
-        await defaultUserApp.navigate.verifyUrl(`${baseURL}${path}`);
-    })
+for (const { title, path } of testCases) {
+  test(`verify navigation to '${title}' page`, async ({
+    defaultUserApp,
+    baseURL,
+  }) => {
+    await defaultUserApp.navigate.goto("/");
+    await defaultUserApp.navigate.openMenuItem(title);
+    await defaultUserApp.navigate.verifyUrl(`${baseURL}${path}`);
+  });
 }
